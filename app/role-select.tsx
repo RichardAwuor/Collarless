673806666659b8290cb742fcfd4ed59af84fa985
natively.dev <api@natively.dev>
2026-01-25 09:58:58 +0000

    style: Arrange role selection boxes vertically

--- app/role-select.tsx
+++ app/role-select.tsx
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
     letterSpacing: 1,
   },
   boxesContainer: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     gap: 16,
     width: '100%',
     justifyContent: 'center',
